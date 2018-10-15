2596125abf7bd129465db82b55afee52443057e8
Max Kugland <maxkugland@kaufhof.de>
2018-10-15 13:58:06 +0200

    fetch in batches, process and write each batch immediately, prefetch the next batch update progress in the console on the same line make the fetchSize configurable

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,14 +1,12 @@
 package example
 
-import com.datastax.driver.core.{ResultSet, ResultSetFuture, Row, Session}
+import com.datastax.driver.core._
 import com.google.common.util.concurrent.{FutureCallback, Futures}
 import org.rogach.scallop._
-import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.json.{JsObject, Json}
 
 import scala.collection.JavaConversions._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success, Try}
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
@@ -17,6 +15,7 @@ class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val table = opt[String](required = true, descr = "The name of the table")
   val port = opt[Int](default = Some(9042), descr = "Optional, the port, default value is 9042")
   val progress = opt[Boolean](default = Some(false), descr = "Print the progress to stderr, defaults to false")
+  val fetchSize = opt[Int](default = Some(3000), descr = "The amount of rows which is retrieved simultaneously. Defaults to 3000")
 
   verify()
 }
@@ -31,19 +30,24 @@ object Main {
       table <- conf.table.get
       port <- conf.port.get
       progress <- conf.progress.get
+      fetchSize <- conf.fetchSize.get
     } yield {
-      if(progress) Console.err.println(s"Connecting to cassandra.")
+      if (progress) updateProgress("Connecting to cassandra.")
 
-      val (_, session) = Cassandra(hosts, keyspace, port)
+      val (cluster, session) = Cassandra(hosts, keyspace, port)
       session.execute(s"use $keyspace")
 
-      Await.result(executeQuery(session, table, progress), Duration.Inf)
+      if (progress) updateProgress(s"Connected to cassandra '${cluster.getClusterName}'")
+
+      val start = System.currentTimeMillis()
+      executeQuery(session, table, fetchSize, progress)
+      if(progress) Console.err.println(s" \nTook ${(System.currentTimeMillis() - start) / 1000}s\n")
     }
 
     System.exit(0)
   }
 
-  def executeQuery(session: Session, table: String, progress: Boolean) = {
+  def executeQuery(session: Session, table: String, fetchSize: Int, progress: Boolean) = {
     val columnValues = (row: Row) => {
       row.getColumnDefinitions.iterator().map { definition =>
         Column(definition.getName, row.getString(definition.getName))
@@ -76,24 +80,31 @@ object Main {
       }
     }
 
-    val prettyPrint = (values: Iterable[JsValue]) => {
-      values.map(Json.prettyPrint).foreach(println)
-    }
+    if (progress) updateProgress("Execute query.")
 
-    if (progress) Console.err.println(s"Execute query.")
-    session.executeAsync(s"select json * from $table;").map { result =>
-      result.zipWithIndex.flatMap { case (row, index) =>
-        if (progress) Console.err.print(s"$index.")
-        columnValues.andThen(columns2Json)(row)
-      }
-    }.map(prettyPrint)
+    val statement = new SimpleStatement(s"select json * from $table;").setFetchSize(fetchSize)
+    val rs = session.execute(statement)
+    rs.toIterator.zipWithIndex.flatMap { case (row, index) =>
+      if (rs.getAvailableWithoutFetching < statement.getFetchSize / 2 && !rs.isFullyFetched) rs.fetchMoreResults()
+      if (progress) updateProgress(s"$index rows.")
+      columnValues.andThen(columns2Json)(row)
+    }.foreach { json =>
+      Console.println(Json.prettyPrint(json))
+    }
+  }
+
+  def updateProgress(s: String) = {
+    val max = 80
+    val sliced = s.slice(0, max)
+    val diff = max - sliced.length
+    val output = sliced + (0 to diff).map(_ => " ").mkString
+    Console.err.print(s"$output\r")
   }
 
   implicit def toScalaFuture(resultSet: ResultSetFuture): Future[ResultSet] = {
     val promise = Promise[ResultSet]
     Futures.addCallback[ResultSet](resultSet, new FutureCallback[ResultSet] {
       override def onSuccess(result: ResultSet): Unit = promise.success(result)
-
       override def onFailure(t: Throwable): Unit = promise.failure(t)
     })
     promise.future
