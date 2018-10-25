efbcde144807a1a7a411afaf513d8d44d2af084b
Max Kugland <maxkugland@gmail.com>
2018-10-25 21:39:51 +0200

    When outputting progress information, just erase the last output instead of using a default line length, which is not suitable for all terminal sizes

--- src/main/scala/example/Output.scala
+++ src/main/scala/example/Output.scala
@@ -1,8 +1,10 @@
 package example
 
 object Output {
+  var lastLength = 0
   def apply(s: String) = {
-    val max = 120
+    val max = lastLength
+    lastLength = s.length
     val sliced = s.slice(0, max)
     val diff = max - sliced.length
     val output = sliced + (0 to diff).map(_ => " ").mkString
