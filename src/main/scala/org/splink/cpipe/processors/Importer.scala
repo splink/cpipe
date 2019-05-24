9d8e720923a0758dfc7616a525b26e2b6f670b54
Jan De Bleser <jan.debleser@restore.eu>
2019-05-24 17:42:22 +0200

    Revert "Improving load speed"
    This reverts commit 9647a029

--- src/main/scala/org/splink/cpipe/processors/Importer.scala
+++ src/main/scala/org/splink/cpipe/processors/Importer.scala
@@ -1,16 +1,10 @@
 package org.splink.cpipe.processors
 
-import java.util.concurrent.TimeUnit
-
 import com.datastax.driver.core.Session
-import org.splink.cpipe.config.Config
 import org.splink.cpipe.{JsonFrame, Output, Rps}
+import org.splink.cpipe.config.Config
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 import scala.io.Source
-
 
 class Importer extends Processor {
 
@@ -20,19 +14,15 @@ class Importer extends Processor {
 
   override def process(session: Session, config: Config): Int = {
     val frame = new JsonFrame()
-    Source.stdin.getLines().flatMap { line =>
-      frame.push(line.toCharArray)
-    }.grouped(500).foreach { group: Iterable[String] =>
-      group.map { jsonStr =>
-        Future {
-          string2Json(jsonStr).map { json =>
-            rps.compute()
-            if (config.flags.showProgress) Output.update(s"${rps.count} rows at $rps rows/sec.")
-            session.execute(json2Query(json, config.selection.table))
-          }.get
+    Source.stdin.getLines().foreach { line =>
+      frame.push(line.toCharArray).foreach { result =>
+        string2Json(result).map { json =>
+
+          rps.compute()
+          if (config.flags.showProgress) Output.update(s"${rps.count} rows at $rps rows/sec.")
+
+          session.execute(json2Query(json, config.selection.table))
         }
-      }.foreach { future =>
-        Await.ready(future, Duration(10, TimeUnit.SECONDS)).recover{case e: Exception =>println(e)}
       }
     }
 
