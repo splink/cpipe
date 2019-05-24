9647a0293503edafb6f687745a85b22ceb863dae
Jan De Bleser <jan.debleser@restore.eu>
2019-05-24 15:11:45 +0200

    Improving load speed

--- src/main/scala/org/splink/cpipe/processors/Importer.scala
+++ src/main/scala/org/splink/cpipe/processors/Importer.scala
@@ -1,10 +1,16 @@
 package org.splink.cpipe.processors
 
-import com.datastax.driver.core.Session
-import org.splink.cpipe.{JsonFrame, Output, Rps}
-import org.splink.cpipe.config.Config
+import java.util.concurrent.TimeUnit
 
+import com.datastax.driver.core.Session
+import org.splink.cpipe.config.Config
+import org.splink.cpipe.{JsonFrame, Output, Rps}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 import scala.io.Source
+
 
 class Importer extends Processor {
 
@@ -14,15 +20,19 @@ class Importer extends Processor {
 
   override def process(session: Session, config: Config): Int = {
     val frame = new JsonFrame()
-    Source.stdin.getLines().foreach { line =>
-      frame.push(line.toCharArray).foreach { result =>
-        string2Json(result).map { json =>
-
-          rps.compute()
-          if (config.flags.showProgress) Output.update(s"${rps.count} rows at $rps rows/sec.")
-
-          session.execute(json2Query(json, config.selection.table))
+    Source.stdin.getLines().flatMap { line =>
+      frame.push(line.toCharArray)
+    }.grouped(500).foreach { group: Iterable[String] =>
+      group.map { jsonStr =>
+        Future {
+          string2Json(jsonStr).map { json =>
+            rps.compute()
+            if (config.flags.showProgress) Output.update(s"${rps.count} rows at $rps rows/sec.")
+            session.execute(json2Query(json, config.selection.table))
+          }.get
         }
+      }.foreach { future =>
+        Await.ready(future, Duration(10, TimeUnit.SECONDS)).recover{case e: Exception =>println(e)}
       }
     }
 
